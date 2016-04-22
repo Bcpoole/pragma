13f08eac10efa90c92d51c9057d9fcd6c4fe21bb
Bcpoole <brandoncpoole@gmail.com>
2016-04-22 00:20:28 -0500

    added environments. Copied bash files to main directory

---
+++ Environment.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace dpl {
+  public class Environment {
+    public Environment() {
+
+    }
+
+    public Node Create() {
+      return Extend(null, null, null);
+    }
+
+    public string Lookup(string id, Node env) {
+      while (env != null) {
+        var table = Car(env);
+        var vars = Car(table);
+        var vals = Cdr(table);
+
+        while (vars != null) {
+          if (id == Car(vars).Value.sval) {
+            return Car(vals).Value.GetValue();
+          }
+          vars = Cdr(vars);
+          vals = Cdr(vars);
+        }
+        env = Cdr(env);
+      }
+
+      throw new Exception(String.Format("variable '{0}' is undefined!", id));
+    }
+
+    public void Update(string id, Node env, string val) {
+      while (env != null) {
+        var vars = Car(env);
+        var vals = Cadr(env);
+        while (vars != null) {
+          if (id == Car(vars).Value.sval) {
+            Car(vals).Value.SetValue(val);
+            return;
+          }
+          vars = Cdr(vars);
+          vals = Cdr(vars);
+        }
+        env = Cdr(Cdr(env));
+      }
+
+      throw new Exception(String.Format("variable '{0}' is undefined!", id));
+    }
+
+    public Node Insert(Node variable, Node val, Node env) {
+      var table = Car(env);
+      SetCar(table, Cons(CreateJOINNode(), variable, Car(table)));
+      SetCdr(table, Cons(CreateJOINNode(), val, Cdr(table)));
+      return val;
+    }
+
+    public Node Extend(Node vars, Node vals, Node env) {
+      return Cons(env, vars,
+          Cons(CreateJOINNode(), vals, Cons(
+              CreateJOINNode(), env, null)));
+    }
+
+    //cons functions
+    public Node Cons(Node val, Node left, Node right) {
+      val.Left = left;
+      val.Right = right;
+
+      return val;
+    }
+
+    public Node Car(Node cons) {
+      return cons.Left;
+    }
+    public void SetCar(Node cons, Node val) {
+      cons.Left = val;
+    }
+
+    public Node Cdr(Node cons) {
+      return cons.Right;
+    }
+    public void SetCdr(Node cons, Node val) {
+      cons.Right = val;
+    }
+
+    public Node Cadr(Node cons) {
+      return cons.Right.Left;
+    }
+
+    public Node Caddr(Node cons) {
+      return cons.Right.Right.Left;
+    }
+
+    private Node CreateJOINNode() {
+      return new Node(new Lexeme("JOIN"));
+    }
+  }
+}
--- Lexeme.cs
+++ Lexeme.cs
@@ -7,21 +7,21 @@ namespace dpl {
     public int ival;
     public string sval;
     //real rval;
-    
+
     public Lexeme(string type) {
       this.type = type;
     }
-    
+
     public Lexeme(string type, int ival) {
       this.type = type;
       this.ival = ival;
     }
-    
+
     public Lexeme(string type, string sval) {
       this.type = type;
       this.sval = sval;
     }
-    
+
     public void Print() {
       switch (type) {
         case "STRING":
@@ -33,6 +33,28 @@ namespace dpl {
         default:
           Console.WriteLine(String.Format("type : {0}", type));
           break;
+      }
+    }
+
+    public string GetValue() {
+      if (type == "STRING") {
+        return sval;
+      }
+      else if (type == "INTEGER") {
+        return ival.ToString();
+      }
+      throw new Exception("Invalid value type");
+    }
+
+    public void SetValue(string val) {
+      if (type == "STRING") {
+        sval = val;
+      }
+      else if (type == "INTEGER") {
+        ival = Convert.ToInt32(val);
+      }
+      else {
+        throw new Exception("Invalid value type");
       }
     }
   }
